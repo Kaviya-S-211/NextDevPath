496757599427fbe406ff356f31a46cf664c4de7e
Kaviya S <kaviya21103@gmail.com>
2025-07-06 13:55:54 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/NextDevPath/sw.js', { scope: '/NextDevPath/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
